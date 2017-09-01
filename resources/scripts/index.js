b2a57b00a39b9a471eace0f37c365612df0febdb
Stephanie Asmar <sasmar8@gmail.com>
2017-09-01 14:16:10 -0400

    fixes navbar html/css; starts javascripts

---
+++ resources/scripts/index.js
@@ -0,0 +1,30 @@
+// when hamburger menu icon is clicked, the hamburger icon hids, the exit icon shows and the menu-container shows slowly
+function clickMenuShow() {
+    $HAMBURGER.click(function () {
+        $EXIT_ICON.show();
+        $(this).hide();
+        $(".myAtlas-logo").hide("slow");
+        $MENU_CONTAINER.show("slow");
+    });
+}
+// when exit icon is clicked, the exit icon hids, the hamburger menu shows, and the menu-container hids slowly
+function clickExitButton() {
+    $EXIT_ICON.click(function () {
+        $HAMBURGER.show();
+        $(this).hide();
+        $(".myAtlas-logo").show("slow");
+        $MENU_CONTAINER.hide("slow");
+    });
+
+}
+
+function addAboutListeners() {
+    clickMenuShow();
+    clickExitButton();
+}
+
+
+// allows document to load before functions are called
+$(document).ready(function() {
+    addAboutListeners();
+});
